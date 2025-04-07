0a0f25f09d038089848e30108053e8c1515bf1e2
sariah-mcintyre <sariah.mcintyre@gmail.com>
2025-04-07 17:41:04 -0600

    Add files via upload

---
+++ elevator_controller_fsm.vhd
@@ -0,0 +1,185 @@
+--+----------------------------------------------------------------------------
+--| 
+--| COPYRIGHT 2018 United States Air Force Academy All rights reserved.
+--| 
+--| United States Air Force Academy     __  _______ ___    _________ 
+--| Dept of Electrical &               / / / / ___//   |  / ____/   |
+--| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
+--| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
+--| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
+--| 
+--| ---------------------------------------------------------------------------
+--|
+--| FILENAME      : MooreElevatorController.vhd
+--| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, Capt Brian Yarbrough, ***YourName***
+--| CREATED       : 03/2018 Last Modified on 06/24/2020
+--| DESCRIPTION   : This file implements the ICE5 Basic elevator controller (Moore Machine)
+--|
+--|  The system is specified as follows:
+--|   - The elevator controller will traverse four floors (numbered 1 to 4).
+--|   - It has two external inputs, i_up_down and i_stop.
+--|   - When i_up_down is active and i_stop is inactive, the elevator will move up 
+--|			until it reaches the top floor (one floor per clock, of course).
+--|   - When i_up_down is inactive and i_stop is inactive, the elevator will move down 
+--|			until it reaches the bottom floor (one floor per clock).
+--|   - When i_stop is active, the system stops at the current floor.  
+--|   - When the elevator is at the top floor, it will stay there until i_up_down 
+--|			goes inactive while i_stop is inactive.  Likewise, it will remain at the bottom 
+--|			until told to go up and i_stop is inactive.  
+--|   - The system should output the floor it is on (1 - 4) as a four-bit binary number.
+--|   - i_reset synchronously puts the FSM into state Floor 2.
+--|
+--|		Inputs:   i_clk     --> elevator clk
+--|				  i_reset   --> reset signal
+--|				  i_stop	--> signal tells elevator to stop moving
+--|				  i_up_down	--> signal controls elavotor 1=up, 0=down
+--|
+--|		Outputs:  o_floor (3:0)	--> 4-bit signal  indicating elevator's floor
+--|  
+--|
+--+----------------------------------------------------------------------------
+--|
+--| REQUIRED FILES :
+--|
+--|    Libraries : ieee
+--|    Packages  : std_logic_1164, numeric_std, unisim
+--|    Files     : None
+--|
+--+----------------------------------------------------------------------------
+--|
+--| NAMING CONVENSIONS :
+--|
+--|    xb_<port name>           = off-chip bidirectional port ( _pads file )
+--|    xi_<port name>           = off-chip input port         ( _pads file )
+--|    xo_<port name>           = off-chip output port        ( _pads file )
+--|    b_<port name>            = on-chip bidirectional port
+--|    i_<port name>            = on-chip input port
+--|    o_<port name>            = on-chip output port
+--|    c_<signal name>          = combinatorial signal
+--|    f_<signal name>          = synchronous signal
+--|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
+--|    <signal name>_n          = active low signal
+--|    w_<signal name>          = top level wiring signal
+--|    g_<generic name>         = generic
+--|    k_<constant name>        = constant
+--|    v_<variable name>        = variable
+--|    sm_<state machine type>  = state machine type definition
+--|    s_<signal name>          = state name
+--|
+--+----------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity elevator_controller_fsm is
+    Port ( i_clk     : in  STD_LOGIC;
+           i_reset   : in  STD_LOGIC;
+           i_stop    : in  STD_LOGIC;
+           i_up_down : in  STD_LOGIC;
+           o_floor   : out STD_LOGIC_VECTOR (3 downto 0)		   
+		 );
+end elevator_controller_fsm;
+
+ 
+architecture Behavioral of elevator_controller_fsm is
+
+    -- Below you create a new variable type! You also define what values that 
+    -- variable type can take on. Now you can assign a signal as 
+    -- "sm_floor" the same way you'd assign a signal as std_logic
+	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);
+	
+	-- Here you create variables that can take on the values defined above. Neat!	
+	signal f_Q, f_Q_next: sm_floor;
+
+begin
+
+	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
+	
+	-- Next State Logic
+
+
+
+next_state_logic: process(f_Q, i_up_down, i_stop)
+begin
+    case f_Q is
+        when s_floor1 =>
+            if i_up_down = '1' then
+                f_Q_next <= s_floor2;
+            else
+                f_Q_next <= s_floor1;
+            end if;
+        when s_floor2 =>
+            if i_stop = '1' then
+                f_Q_next <= s_floor2;
+            elsif i_up_down = '1' then
+                f_Q_next <= s_floor3;
+            else
+                f_Q_next <= s_floor1;
+            end if;
+        when s_floor3 =>
+            if i_stop = '1' then
+                f_Q_next <= s_floor3;
+            elsif i_up_down = '1' then
+                f_Q_next <= s_floor4;
+            else
+                f_Q_next <= s_floor2;
+            end if;
+        when s_floor4 =>
+            if i_stop = '1' then
+                f_Q_next <= s_floor4;
+            elsif i_up_down = '0' then
+                f_Q_next <= s_floor3;
+            else
+                f_Q_next <= s_floor4;
+            end if;
+    end case;
+end process;
+                
+                
+                
+    with f_Q select        
+    o_floor <= "0001" when s_floor1,
+                "0010" when s_floor2,
+                "0011" when s_floor3,
+                "0100" when s_floor4,
+                
+                "0001" when others;
+                
+                
+                
+               
+           
+          
+            
+            
+        
+        	   
+  
+	-- Output logic
+
+	-------------------------------------------------------------------------------------------------------
+	
+	-- PROCESSES ------------------------------------------------------------------------------------------	
+	
+	-- State register ------------
+	
+	
+	stateregister_process: process(i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if i_reset = '1' then
+                f_Q <= s_floor2;
+            elsif i_stop = '0' then
+                f_Q <= f_Q_next;
+            end if;
+        end if;
+    end process;
+	
+	
+	-------------------------------------------------------------------------------------------------------
+	
+	
+
+
+
+end Behavioral;
+
---
+++ elevator_controller_fsm_tb.vhd
@@ -0,0 +1,150 @@
+--+----------------------------------------------------------------------------
+--| 
+--| COPYRIGHT 2017 United States Air Force Academy All rights reserved.
+--| 
+--| United States Air Force Academy     __  _______ ___    _________ 
+--| Dept of Electrical &               / / / / ___//   |  / ____/   |
+--| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
+--| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
+--| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
+--| 
+--| ---------------------------------------------------------------------------
+--|
+--| FILENAME      : MooreElevatorController_tb.vhd (TEST BENCH)
+--| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, **Your Name Here**
+--| CREATED       : 03/2017 Last modified on 06/24/2020
+--| DESCRIPTION   : This file tests the Moore elevator controller module
+--|
+--| DOCUMENTATION : None
+--|
+--+----------------------------------------------------------------------------
+--|
+--| REQUIRED FILES :
+--|
+--|    Libraries : ieee
+--|    Packages  : std_logic_1164, numeric_std, unisim
+--|    Files     : MooreElevatorController.vhd
+--|
+--+----------------------------------------------------------------------------
+--|
+--| NAMING CONVENSIONS :
+--|
+--|    xb_<port name>           = off-chip bidirectional port ( _pads file )
+--|    xi_<port name>           = off-chip input port         ( _pads file )
+--|    xo_<port name>           = off-chip output port        ( _pads file )
+--|    b_<port name>            = on-chip bidirectional port
+--|    i_<port name>            = on-chip input port
+--|    o_<port name>            = on-chip output port
+--|    c_<signal name>          = combinatorial signal
+--|    f_<signal name>          = synchronous signal
+--|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
+--|    <signal name>_n          = active low signal
+--|    w_<signal name>          = top level wiring signal
+--|    g_<generic name>         = generic
+--|    k_<constant name>        = constant
+--|    v_<variable name>        = variable
+--|    sm_<state machine type>  = state machine type definition
+--|    s_<signal name>          = state name
+--|
+--+----------------------------------------------------------------------------
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+ 
+entity elevator_controller_fsm_tb is
+end elevator_controller_fsm_tb;
+
+architecture test_bench of elevator_controller_fsm_tb is 
+	
+	component elevator_controller_fsm is
+		Port ( i_clk 	 : in  STD_LOGIC;
+			   i_reset 	 : in  STD_LOGIC; -- synchronous
+			   i_stop 	 : in  STD_LOGIC;
+			   i_up_down : in  STD_LOGIC;
+			   o_floor 	 : out STD_LOGIC_VECTOR (3 downto 0));
+	end component elevator_controller_fsm;
+	
+	-- test signals
+	signal w_clk, w_reset, w_stop, w_up_down : std_logic := '0';
+	signal w_floor : std_logic_vector(3 downto 0) := (others => '0');
+  
+	-- 50 MHz clock
+	constant k_clk_period : time := 20 ns;
+	
+begin
+	-- PORT MAPS ----------------------------------------
+
+	uut_inst : elevator_controller_fsm port map (
+		i_clk     => w_clk,
+		i_reset   => w_reset,
+		i_stop    => w_stop,
+		i_up_down => w_up_down,
+		o_floor   => w_floor
+	);
+	-----------------------------------------------------
+	
+	-- PROCESSES ----------------------------------------
+	
+	-- Clock Process ------------------------------------
+	clk_process : process
+	begin
+		w_clk <= '0';
+		wait for k_clk_period/2;
+		
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process clk_process;
+	
+	
+	-- Test Plan Process --------------------------------
+	 test_process : process 
+    begin
+        -- Reset to initial state (floor 2)
+        w_reset <= '1';
+        w_stop <= '1';
+        wait for k_clk_period;
+        w_reset <= '0';
+        wait for k_clk_period;
+        assert w_floor = x"2" report "bad reset" severity failure;
+
+        -- Active UP signal
+        w_up_down <= '1'; 
+        w_stop <= '0';  
+        wait for k_clk_period;
+        assert w_floor = x"3" report "bad up from floor2" severity failure;
+
+        -- Wait on a floor
+        w_stop <= '1';  
+        wait for k_clk_period * 2;
+        assert w_floor = x"3" report "bad wait on floor3" severity failure;
+
+        -- Go up again
+        w_stop <= '0';  
+        wait for k_clk_period;
+        assert w_floor = x"4" report "bad up from floor3" severity failure;
+
+        -- Go back down one floor
+        w_up_down <= '0'; 
+        wait for k_clk_period;
+        assert w_floor = x"3" report "bad down from floor4" severity failure;
+
+        -- Go back up
+        w_up_down <= '1'; 
+        wait for k_clk_period;
+        assert w_floor = x"4" report "bad up from floor3" severity failure;
+
+        -- Stop at the top
+        w_stop <= '1';  
+        wait for k_clk_period * 2;
+        assert w_floor = x"4" report "bad wait on floor4" severity failure;
+
+        -- Slide all the way down
+        w_up_down <= '0'; 
+        w_stop <= '0';
+        wait for k_clk_period * 3;
+        assert w_floor = x"1" report "bad slide down to floor1" severity failure;
+
+        wait; -- End of test
+    end process;
+
+end test_bench;
